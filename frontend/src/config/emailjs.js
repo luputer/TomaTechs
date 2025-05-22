36add0bd2b8d3a845f702a3ca4fa94f3010e75ae
yuliagusmy <yuliagusmy@gmail.com>
2025-05-22 12:51:41 +0800

    update email configuration on contact

---
+++ frontend/src/config/emailjs.js
@@ -0,0 +1,5 @@
+export const EMAILJS_CONFIG = {
+  SERVICE_ID: 'service_9mw6a3m',
+  TEMPLATE_ID: 'template_bnajie3',
+  PUBLIC_KEY: 'mVCAzQjGBIy3XK6_K',
+};
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,11 @@
+import emailjs from '@emailjs/browser'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import { registerSW } from 'virtual:pwa-register'
 import App from './App.jsx'
 import { ThemeProvider } from './components/theme-provider.jsx'
+import { EMAILJS_CONFIG } from './config/emailjs'
 import { AuthProvider } from './context/AuthContext.jsx'
 import './index.css'
 
@@ -18,6 +20,9 @@ const updateSW = registerSW({
     console.log('App ready to work offline')
   },
 })
+
+// Initialize EmailJS
+emailjs.init(EMAILJS_CONFIG.PUBLIC_KEY);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
